46a373ab173ab50c1dd250e8f73dc58d0504e98c
elenaberegova <elenaberegova@proton.me>
2026-07-26 20:42:14 +0300

    Add csvReader module to read CSV files

---
+++ src/csvReader.js
@@ -0,0 +1,21 @@
+const fs=require("fs");
+
+function read(file){
+
+    return fs
+
+        .readFileSync(
+
+            file,
+
+            "utf8"
+
+        );
+
+}
+
+module.exports={
+
+    read
+
+};
